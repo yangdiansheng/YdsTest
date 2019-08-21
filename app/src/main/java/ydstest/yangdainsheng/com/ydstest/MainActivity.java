32841e8f968fe641f08656dafecfb0955cb63d2d
yangdiansheng <yangdiansheng@foxmail.com>
2019-08-21 18:37:30 +0800

    rxjava

--- app/src/main/java/ydstest/yangdainsheng/com/ydstest/MainActivity.java
+++ app/src/main/java/ydstest/yangdainsheng/com/ydstest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -20,6 +21,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Observer;
 import rx.Scheduler;
 import rx.Subscriber;
 import rx.schedulers.Schedulers;
@@ -55,6 +58,75 @@ public class MainActivity extends Activity {
          */
         request4();
 
+    }
+
+    @OnClick(R.id.tv_rxjava)
+    void rxjava(){
+        /**
+         * 异步
+         * 简洁
+         * 扩展观察者实现
+         */
+
+        //观察者
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.i("yyy","onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i("yyy","onError");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i("yyy","onNext = " +  s);
+            }
+        };
+
+        Subscriber<String> subscriber = new Subscriber<String>() {
+
+            /**
+             * 这是 Subscriber 增加的方法。它会在 subscribe 刚开始，而事件还未发送之前被调用，可以用于做一些准备工作，
+             * 例如数据的清零或重置。这是一个可选方法，默认情况下它的实现为空。需要注意的是，如果对准备工作的线程有要求（例如弹出一个显示进度的对话框，这必须在主线程执行），
+             * onStart() 就不适用了，因为它总是在 subscribe 所发生的线程被调用，而不能指定线程。要在指定的线程来做准备工作
+             */
+            @Override
+            public void onStart() {
+                super.onStart();
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+        };
+
+        //被观察者
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        });
+
+        //订阅
+        observable.subscribe(observer);
     }
 
     //自定义CallAdapter
